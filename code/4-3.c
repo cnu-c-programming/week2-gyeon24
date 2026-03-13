5fd2b072741e956b86ce772f6e78b0fc54ad9b9d
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-03-13 13:35:25 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main()
-{
-
-    return 0;
+bool test(int n) {
+    if (n <= 1) return false;
+    
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    
+    return true;
 }
 
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    if (test(n)) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
+    
+    return 0;
+}

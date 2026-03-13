d9fb8ae5b7d46633d0eed14eada08b25dcabaed1
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-03-13 10:02:45 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 
-/*
-    int : 100
-hex : 64
-oct : 144
-char : c
-string : hello world!
-float : 3.141592
-exp : 3.141592e+00
-    */
-
 int main()
 {
-    int v1 = 100;
-    printf("int : %d\n", v1);
-    printf("hex : %x\n", v1);
-    printf("oct : %o\n", v1);
+    int var1 = 100;
+    printf("int : %d\n", var1);
+    printf("hex : %x\n", var1);
+    printf("oct : %o\n", var1);
 
-    char v2 = 'c';
-    printf("char : %c\n", v2);
+    char var2 = 'c';
+    printf("char : %c\n", var2);
 
-    char* v3 = "hello world!";
-    printf("string : %s\n", v3);
+    char* var3 = "hello world!";
+    printf("string : %s\n", var3);
 
-    float v4 = 3.141592;
-    printf("float : %f\n", v4);
-    printf("exp : %e\n", v4);
+    float var4 = 3.141592;
+    printf("float : %f\n", var4);
+    printf("exp : %e\n", var4);
 
     return 0;
 }

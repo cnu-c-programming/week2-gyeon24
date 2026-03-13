964e40a90f6c369f9f9ec799f33c4489b91beb72
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-03-13 09:49:40 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/*
+    int: 1 2 3
+    out: 1 2 3.000000
+    */
 int main()
 {
-
-
+    int v1 = 1, v2 = 2, v3 = 3;
+    printf("%d %d %.6f\n", v1, v2, (double)v3);
+    
     return 0;
 }

b78f3a21ae42bb39ad9775f533949077f994b003
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-03-13 09:58:39 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
-
+/*
+    char: 1
+short: 2
+int: 4
+unsigned int: 4
+long: 8
+long long: 8
+float: 4
+    */
 int main()
 {
-
+    printf("char: %zu\n", sizeof(char));
+    printf("short: %zu\n", sizeof(short));
+    printf("int: %zu\n", sizeof(int));
+    printf("unsigned int: %zu\n", sizeof(unsigned int));
+    printf("long: zu\n", sizeof(long));
+    printf("long long: %zu\n", sizeof(long long));
+    printf("float: %zu\n", sizeof(float));
+    
     return 0;
 }

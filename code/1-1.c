dbab56f2dfd9ca7ddebcd0287949f6e7516e3116
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-03-13 09:26:50 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
+/*
+    int : 100
+hex : 64
+oct : 144
+char : c
+string : hello world!
+float : 3.141592
+exp : 3.141592e+00
+    */
+
 int main()
 {
+    int v1 = 100;
+    printf("int : %d\n", v1);
+    printf("hex : %x\n", v1);
+    printf("oct : %o\n", v1);
+
+    char v2 = 'c';
+    printf("char : %c\n", v2);
+
+    char* v3 = "hello world!";
+    printf("string : %s\n", v3);
+
+    float v4 = 3.141592;
+    printf("float : %f\n", v4);
+    printf("exp : %e\n", v4);
 
     return 0;
 }
